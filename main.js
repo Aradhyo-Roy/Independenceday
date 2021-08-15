840167c8f5c0b17ab849d45e2a1831cbbcb3031c
Aradhyo Roy <83859573+Aradhyo-Roy@users.noreply.github.com>
2021-08-15 10:17:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function changelabel(){
+    document.getElementById("showit").innerHTML="Jai hind";
+    document.getElementById("l").style.backgroundColor="blue";
+    document.getElementById("dl").style.backgroundColor="white";
+}
+function dontchangelabel(){
+    document.getElementById("showit").innerHTML="Ok";
+    document.getElementById("dl").style.backgroundColor="red";
+    document.getElementById("l").style.backgroundColor="white";
+}
